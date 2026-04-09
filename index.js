2a7b8a759d8d91d70f3baeb32fdab8b7daf6663d
Izuku-mi <151708571+izuku-mii@users.noreply.github.com>
2026-04-09 12:22:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,14 @@
     const path = require('path');
     const chalk = require('chalk');
     const fs = require('node:fs');
-    const PORT = process.env.PORT || 4000   
-    const app = express();
+    const PORT = process.env.PORT || 4000
+    const app = express()
     app.set('trust proxy', true);
     app.set('json spaces', 2);
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(require('./lib/logApiRequest'));
+    app.use('/assets', express.static(path.join(process.cwd(), 'assets')));
     app.use((req, res, next) => {
         const originalJson = res.json;
         res.json = function (data) {
@@ -29,7 +30,7 @@
         };
         next();
     });
-    logger.info('Starting server initialization...');   
+    logger.info('Starting server initialization...');
     logger.info('Loading API endpoints...');
     const allEndpoints = require('./lib/loader').loadEndpointsFromDirectory('api', app);
     console.log('');
@@ -43,28 +44,27 @@
         res.status(200).json({
             endpoints: allEndpoints
         });
-    });
-    app.use('/assets', express.static(path.join(process.cwd(), 'assets')))
+    }); 
     app.use((req, res, next) => {
         logger.info(`404: ${req.method} ${req.path}`);
         res.status(404).json({
             error: 'Huh??'
         })
-    });
+    }); 
     app.use((err, req, res, next) => {
         logger.error(`500: ${err.message}`);
         res.status(500).json({
             error: 'Huh??'
         })
-    });
+    });    
     app.listen(PORT, () => {
         console.log('');
         logger.ready(`Server started successfully`);
-        logger.info(`Local:   ${chalk.cyan(`http://localhost:${PORT}`)}`);
+        logger.info(`Local:   ${chalk.cyan(`http://localhost:${PORT}`)}`);    
         try {
             const { networkInterfaces } = require('os');
             const nets = networkInterfaces();
-            const results = {};          
+            const results = {};       
             for (const name of Object.keys(nets)) {
                 for (const net of nets[name]) {
                     if (net.family === 'IPv4' && !net.internal) {
@@ -74,7 +74,7 @@
                         results[name].push(net.address);
                     }
                 }
-            }            
+            }         
             for (const [, addresses] of Object.entries(results)) {
                 for (const addr of addresses) {
                     logger.info(`Network: ${chalk.cyan(`http://${addr}:${PORT}`)}`);
@@ -82,9 +82,9 @@
             }
         } catch (error) {
             logger.warn(`Cannot detect network interfaces: ${error.message}`);
-        }       
+        }     
         logger.info(`${chalk.dim('Ready for connections')}`);
         console.log('');
-    });    
+    });
     module.exports = app;
 })();
